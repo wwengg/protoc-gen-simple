8f3bd80b901c5d3b7bbe6e6afb0091a55d533fdd
wwengg <wwwwangg@163.com>
2024-07-03 14:37:57 +0800

    version 0.0.7

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"google.golang.org/protobuf/types/pluginpb"
 )
 
-const version = "0.0.4"
+const version = "0.0.7"
 
 func main() {
 	showVersion := flag.Bool("version", false, "print the version and exit")
